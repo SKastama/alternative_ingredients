18657fb553fd21421b5c2e48d53adfd83d3dbda6
Sarah Kastama <sarahkastama@gmail.com>
2021-11-12 10:36:13 -0800

    displays item name, substitutes, and message

--- flask_app/static/js/script.js
+++ flask_app/static/js/script.js
@@ -5,14 +5,34 @@ function search(e){
     fetch('http://localhost:5000/search',{method:'POST',body:form})
         .then(res => res.json() )
         .then(data => {
+            var itemName = document.getElementById('itemName');
+            removeAllChildNodes(itemName);
+            var message = document.getElementById('message');
+            removeAllChildNodes(message);
             var subs = document.getElementById('subs')
             removeAllChildNodes(subs);
-            for (let i=0; i < data.substitutes.length; i++) {
-                let row = document.createElement('tr');
-                let name = document.createElement('td');
-                name.innerHTML = data.substitutes[i];
-                row.appendChild(name);
-                subs.appendChild(row);
+            if (data.status == "success") {
+                // Item name
+                let tableHeader = document.createElement('th');
+                tableHeader.innerHTML = `${capitalizeFirstLetter(data.ingredient)} Substitutes`;
+                itemName.appendChild(tableHeader);
+                // Message
+                let tableMessage = document.createElement('p');
+                tableMessage.innerHTML = capitalizeFirstLetter(data.message);
+                message.appendChild(tableMessage);
+                // Substitutes
+                for (let i=0; i < data.substitutes.length; i++) {
+                    let row = document.createElement('tr');
+                    let name = document.createElement('td');
+                    name.innerHTML = data.substitutes[i];
+                    row.appendChild(name);
+                    subs.appendChild(row);
+                }
+            }
+            else {
+                let tableMessage = document.createElement('p');
+                tableMessage.innerHTML = capitalizeFirstLetter(data.message);
+                message.appendChild(tableMessage);
             }
         })
 }
@@ -20,4 +40,8 @@ function removeAllChildNodes(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
